0cbac064c408dad36b55f71a1ddfefc190001876
Elliot H Smith <ollio22@gmail.com>
2016-04-03 16:43:20 -0400

    updated findMSIT to include a search for LFP files as well as action potential files.

--- findMSIT.m
+++ findMSIT.m
@@ -1,13 +1,23 @@
-function [nevList] = findMSIT(patientID,patientDirectory)
+function [nevList] = findMSIT(patientID,patientDirectory,nsFlag)
 %FINDMSIT finds MSIT data in a patient directory
-%   nevList = findMSIT(patientID,patientDirectory) will search recursively through the directory 
+%   nevList = findMSIT(patientID,patientDirectory) will search recursively through the directory
 % 	in patientDirectory for MSIT data and will return the names of files that have
-%	the trigger structure output from psychToolbox. These nev files will also be copied to 
+%	the trigger structure output from psychToolbox. These nev files will also be copied to
 % 	a directory in the shethLabBackup Data folder called [patientID]_MSIT.
+%
+%   This code will also transfer the associated NS3 file for local field
+%   potential analysis if teh nsFlag is set to 1 (default, 0).
 
-% author: EHS20160210  
+% versionDate: 20160402
+% author: EHS
 
-cd /mnt/mfs/shethLabBackup_nov15/Code/Analysis/
+cd /mnt/mfs/shethLab/Code/Analysis/
+
+% setting defaults.
+if nargin <3
+    nsFlag = false;
+end
+logical nsFlag
 
 % 1) find .nev files in the directory
 dirlist = subdir(fullfile(patientDirectory,'*.nev'));
@@ -19,6 +29,10 @@ addpath(genpath('./NPMK'))
 sessionCount = 0;
 nevList = cell(0);
 nevListStr = char();
+if nsFlag
+    nsList = cell(0);
+    nsListStr = char();
+end
 
 % looping over nev files.
 for fl = 1:length(dirlist)
@@ -32,16 +46,73 @@ for fl = 1:length(dirlist)
         N = sum(triggers==90);
         display(sprintf('found %d trials of MSIT in file %s',N,dirlist(fl).name))
         nevList = vertcat(nevList,dirlist(fl).name);
-	nevListStr = horzcat(nevListStr,' ',dirlist(fl).name);
+        nevListStr = horzcat(nevListStr,' ',dirlist(fl).name);
+        if nsFlag
+            nsList = vertcat(nsList,[dirlist(fl).name(1:end-3) '.ns3']);
+            nsListStr = horzcat(nevListStr,' ',[dirlist(fl).name(1:end-3) '.ns3']);
+        end
     elseif isequal(triggers(1),255) && ~isequal(triggers(2),90)
-	display('There is the start of a task in this file, though it isn't MSIT.')
+        display('There is the start of a task in this file, though it isn't MSIT.')
     else
         display(sprintf('There are triggers in file %s, though they might not be MSIT.',dirlist(fl).name))
     end
 end
 
+display(sprintf('copying NEV files to ~/Data/%s/',patientID))
+eval(['! mkdir ~/Data/' patientID '/'])
+eval(['! cp -v ' nevListStr '  ~/Data/' patientID '/'])
 
-eval(['! mkdir /mnt/mfs/shethLabBackup_nov15/Data/' patientID '/'])
-eval(['! cp -v ' nevListStr '  /mnt/mfs/shethLabBackup_nov15/Data/' patientID '/'])
-
+if nsFlag
+    display(sprintf('copying NS3 files to ~/Data/%s/',patientID))
+    eval(['! mkdir ~/Data/' patientID '/'])
+    eval(['! cp -v ' nsListStr '  ~/Data/' patientID '/'])
 end
+
+return
+
+
+function [sub,fls] = subdir(CurrPath)
+%   SUBDIR  lists (recursive) all subfolders and files under given folder
+%
+%   SUBDIR
+%        returns all subfolder under current path.
+%
+%   P = SUBDIR('directory_name')
+%       stores all subfolders under given directory into a variable 'P'
+%
+%   [P F] = SUBDIR('directory_name')
+%       stores all subfolders under given directory into a
+%       variable 'P' and all filenames into a variable 'F'.
+%       use sort([F{:}]) to get sorted list of all filenames.
+%
+%   See also DIR, CD
+
+%   author:  Elmar Tarajan [Elmar.Tarajan@Mathworks.de]
+%   version: 2.0
+%   date:    07-Dez-2004
+%
+if nargin == 0
+    CurrPath = cd;
+end% if
+
+if nargout == 1
+    sub = subfolder(CurrPath,'');
+else
+    [sub fls] = subfolder(CurrPath,'','');
+end% if
+
+
+function [sub,fls] = subfolder(CurrPath,sub,fls)
+%------------------------------------------------
+tmp = dir(CurrPath);
+tmp = tmp(~ismember({tmp.name},{'.' '..'}));
+for i = {tmp([tmp.isdir]).name}
+    sub{end+1} = [CurrPath '\' i{:}];
+    if nargin==2
+        sub = subfolder(sub{end},sub);
+    else
+        tmp = dir(sub{end});
+        fls{end+1} = {tmp(~[tmp.isdir]).name};
+        [sub fls] = subfolder(sub{end},sub,fls);
+    end% if
+end% if
